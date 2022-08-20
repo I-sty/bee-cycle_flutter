419d1b3b31370062c9c59111e45c2fc757a0ee32
István Szöllősi <szollosi.istv4n@gmail.com>
2022-08-20 19:59:36 +0300

    Created new folder structure (#2)

---
+++ lib/features/bicycle_selector_widget/bicycle_selector_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BicycleSelectorWidget extends StatelessWidget {
+  const BicycleSelectorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("BicycleSelectorWidget");
+  }
+
+}
---
+++ lib/features/welcome_widget/welcome_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WelcomeWidget extends StatelessWidget {
+  const WelcomeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("WelcomeWidget");
+  }
+
+}
